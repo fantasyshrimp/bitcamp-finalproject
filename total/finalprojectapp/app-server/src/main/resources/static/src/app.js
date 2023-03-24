a4da9887b45d933bd3b01b562a2806e36ae3691a
jongkwangyun <yjk0319@gmail.com>
2023-03-24 17:22:30 +0900

    html static 으로 수정. 모달창 변경 전

--- total/finalprojectapp/app-server/src/main/resources/static/src/app.js
+++ total/finalprojectapp/app-server/src/main/resources/static/src/app.js
@@ -48,9 +48,10 @@ function Header() {
             <div className="col"></div>
             <div className="col"></div>
             <div className="col">
-              <a className="nav-link text-light text-end" href="#">
+              {/* <a  href="#" onClick={}>
                 로그인
-              </a>
+              </a> */}
+              <LoginControl />
             </div>
           </div>
         </div>
@@ -86,7 +87,7 @@ function Main() {
           <div className="col">
             <div className="">
               <a className="border text-light p-1" href="#">
-                Try Artify <i class="bi-box"></i>
+                Try Artify <i className="bi-box"></i>
               </a>
             </div>
           </div>
@@ -129,6 +130,99 @@ function Footer() {
       <div className="d-flex align-items-center">created by Someone</div>
     </footer>
   );
+}
+
+function LoginButton(props) {
+  return (
+    <a
+      onClick={props.onClick}
+      className="nav-link text-light text-end"
+      data-bs-toggle="modal"
+      href="#exampleModalToggle"
+      role="button"
+    >
+      로그인
+    </a>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <a
+      onClick={props.onClick}
+      className="nav-link text-light text-end"
+      href="#"
+    >
+      로그아웃
+    </a>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
+
+  handleLoginClick() {
+    // this.setState({ isLoggedIn: true });
+    return (
+      <div
+        className="modal fade"
+        id="exampleModalToggle"
+        aria-hidden="true"
+        aria-labelledby="exampleModalToggleLabel"
+        tabindex="-1"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalToggleLabel">
+                Modal 1
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              Show a second modal and hide this one with the button below.
+            </div>
+            <div className="modal-footer">
+              <button
+                className="btn btn-primary"
+                data-bs-target="#exampleModalToggle2"
+                data-bs-toggle="modal"
+              >
+                Open second modal
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    return (
+      <div>
+        {isLoggedIn ? (
+          <LogoutButton onClick={this.handleLogoutClick} />
+        ) : (
+          <LoginButton onClick={this.handleLoginClick} />
+        )}
+      </div>
+    );
+  }
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
