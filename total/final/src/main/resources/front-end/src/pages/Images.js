c5181f087f248809d48caf8c507999817894ee64
zyoonshin <dwsrh03@gmail.com>
2023-03-28 18:00:22 +0900

    Profile images

---
+++ total/final/src/main/resources/front-end/src/pages/Images.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Images() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="https://storage.googleapis.com/pai-images/1140c9aeeca0410d9644b599aa8bae29.jpeg" />
+          <img src="https://storage.googleapis.com/pai-images/2e1621341a5b419888cd79d9274f4c6b.jpeg" />
+        </div>
+        <div class="col">
+          <img src="https://storage.googleapis.com/pai-images/607ffdcfcd04421b9436e4089506489c.jpeg" />
+          <img src="https://storage.googleapis.com/pai-images/19072de812df4af8817c8dd8c0e284a2.jpeg" />
+        </div>
+        <div class="col">
+          <img src="https://storage.googleapis.com/pai-images/04e7113e04894d489370a4e59f8435b7.jpeg" />
+          <img src="https://storage.googleapis.com/pai-images/fefb6b0c95504877a3671e6e70b37661.jpeg" />
+        </div>
+        <div class="col">
+          <img src="https://storage.googleapis.com/pai-images/1159941b833841f2b9867c0cf29bbc24.jpeg" />
+          <img src="https://storage.googleapis.com/pai-images/3b4db3aa272746a985f2ab0b38b21bf7.jpeg" />
+        </div>
+        <div class="col">
+          <img src="https://storage.googleapis.com/pai-images/812d09f575dc4ee6b68bbbca5d8e4649.jpeg" />
+          <img src="https://storage.googleapis.com/pai-images/c571581ad21645b2b86f4493eaf05da2.jpeg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Images;
