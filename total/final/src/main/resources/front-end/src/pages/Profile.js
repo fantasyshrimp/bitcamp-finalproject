154ddead79a2f198612f7c80e629b779cd366ef5
zyoonshin <dwsrh03@gmail.com>
2023-03-28 18:24:03 +0900

    Profile images

---
+++ total/final/src/main/resources/front-end/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/images.css";
+import Images from "./Images";
+
+function Profile() {
+  return (
+    <div id="body">
+      <Images />
+    </div>
+  );
+}
+
+export default Profile;
