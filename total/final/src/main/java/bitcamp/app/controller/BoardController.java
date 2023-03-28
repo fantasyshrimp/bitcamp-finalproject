88fae13c4bda2ec25c510d319f64ee3ce278c252
ohbyunghyun <960903obh@gmail.com>
2023-03-28 19:47:15 +0900

    피드살짝만듬

---
+++ total/final/src/main/java/bitcamp/app/controller/BoardController.java
@@ -0,0 +1,32 @@
+package bitcamp.app.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import bitcamp.app.service.BoardService;
+import bitcamp.app.vo.Board;
+import bitcamp.app.vo.Member;
+
+@RestController
+// @RequestMapping("/member")
+@RequestMapping("/api")
+public class BoardController {
+  @Autowired private BoardService boardService;
+
+  @PostMapping
+  public void insert(@RequestBody Member member) {
+
+  }
+
+  @GetMapping("/boards")
+  public List<Board> list(String keyword) {
+
+    return boardService.list(keyword);
+  }
+
+}
+
--- total/final/src/main/resources/front-end/src/components/Footer.js
+++ total/final/src/main/resources/front-end/src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Footer() {
-    return (
-      <div id="footer">
-        <p id="small-font">created by Someone</p>
-      </div>
-    );
-  }
+  return (
+    <footer id="footer">
+      <p id="small-font">created by Someone</p>
+    </footer>
+  );
+}
 
 export default Footer;
