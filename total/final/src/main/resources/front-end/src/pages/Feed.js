1c748a825a09694e654b6f8fd7d7f41d1420b6e5
ohbyunghyun <960903obh@gmail.com>
2023-03-29 17:58:25 +0900

    Design: feed

--- total/final/src/main/resources/front-end/src/pages/Feed.js
+++ total/final/src/main/resources/front-end/src/pages/Feed.js
@@ -2,6 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./Feed.css";
 import axios from "axios";
 
+const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>모달창 열기</button>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>모달창 내용</h2>
+            <button onClick={closeModal}>닫기</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 function isScrolledToBottom() {
   return (
     window.scrollY + window.innerHeight >= document.documentElement.scrollHeight
@@ -11,6 +37,16 @@ function isScrolledToBottom() {
 function Tagbar() {
   return (
     <div id="tag-bar">
+      <div id="tag">
+        HOT
+        <div
+          id="tag-image"
+          style={{
+            backgroundImage: `url(/campfire.png)`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </div>
       <div id="tag">Animal</div>
       <div id="tag">Anime</div>
       <div id="tag">Fashion</div>
@@ -64,14 +100,32 @@ function List() {
           }}
         >
           <div id="feed-writer" className="feed-item">
-            <p id="feed-small-font" key={item.writerName}>
-              {item.writerName}
-            </p>
+            <div
+              id="feed-writer-pic"
+              style={{
+                backgroundImage: `url(/logo512.png)`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+            <div id="feed-writer-name">
+              <p id="feed-small-font" key={item.writerName}>
+                {item.writerName}
+              </p>
+            </div>
           </div>
           <div id="feed-like" className="feed-item">
-            <p id="feed-small-font" key={item.likeCnt}>
-              {item.likeCnt}
-            </p>
+            <div id="feed-like-cnt">
+              <p id="feed-small-font-right" key={item.likeCnt}>
+                {item.likeCnt}
+              </p>
+            </div>
+            <div
+              id="feed-like-icon"
+              style={{
+                backgroundImage: `url(/heart.png)`,
+                backgroundSize: "cover",
+              }}
+            ></div>
           </div>
           <div id="feed-content" className="feed-item">
             <p id="feed-small-font" key={item.originContent}>
