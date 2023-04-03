57a40d55e008f9551d6ab9d99933f5f52c0583b4
zyoonshin <dwsrh03@gmail.com>
2023-04-03 16:48:30 +0900

    edit data.js

--- total/front-end/src/pages/Main/data.js
+++ total/front-end/src/pages/Main/data.js
@@ -1,23 +1,23 @@
 const imageSlide = [
   {
     url: "https://e0.pxfuel.com/wallpapers/625/372/desktop-wallpaper-landscape-pixel-art-resolution-artist-and-background-1600-x-900-landscape.jpg",
-    title: "1st",
-    body: "first",
+    title: "AI 그림이란?",
+    body: "최근 주목받고 있는 AI 기술을 활용해 그림을 생성하는 서비스입니다.",
   },
   {
     url: "https://e0.pxfuel.com/wallpapers/374/744/desktop-wallpaper-spring-1600-x-900-landscape.jpg",
-    title: "2nd",
-    body: "second",
+    title: "Artify",
+    body: "Artify를 이용하여 자신만의 AI 이미지를 만들고 자랑해보세요!",
   },
   {
     url: "https://e0.pxfuel.com/wallpapers/857/594/desktop-wallpaper-nature-nature-beach-nature-1600-x-900-landscape.jpg",
-    title: "3rd",
-    body: "third",
+    title: "Artify 사용 방법",
+    body: "그리고 싶은 그림을 상상하여 프롬포트 창에 입력하고 Enter키를 누르세요.",
   },
   {
     url: "https://wallpapercave.com/wp/wp4657259.jpg",
-    title: "4th",
-    body: "fourth",
+    title: "Artify 사용 Tip!",
+    body: "자세히 적을수록 원하는 그림이 생성될 확률이 높아집니다.",
   },
 ];
 
