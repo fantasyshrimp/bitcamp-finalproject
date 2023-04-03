bfce0f73367d81f23107fa6f29e2f8edd5effe6b
zyoonshin <dwsrh03@gmail.com>
2023-04-03 14:16:24 +0900

    Main-BackgroundSlider upload

--- total/final/src/main/resources/front-end/src/App.js
+++ total/final/src/main/resources/front-end/src/App.js
@@ -4,9 +4,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+<<<<<<< Updated upstream:total/final/src/main/resources/front-end/src/App.js
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
 import Test1 from "./pages/Test1";
+=======
+import Feed from "./pages/Feed/Feed";
+import Profile from "./pages/profile/Profile";
+import BackgroundSlider from "./pages/Main/BackgroundSlider";
+>>>>>>> Stashed changes:total/front-end/src/App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -18,7 +24,11 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/Feed" element={<Feed />}></Route>
           <Route path="/Profile" element={<Profile />}></Route>
+<<<<<<< Updated upstream:total/final/src/main/resources/front-end/src/App.js
           <Route path="/Test1" element={<Test1 />}></Route>
+=======
+          <Route path="/BackgroundSlider" element={<BackgroundSlider />} />
+>>>>>>> Stashed changes:total/front-end/src/App.js
         </Routes>
         <Footer />
       </BrowserRouter>
