364381fa1c6fc0780165d4505239c5edde3424a5
das7945 <das7945@naver.com>
2023-04-04 21:32:28 +0900

    FAQ 진행중 DB TEST완료

--- total/front-end/src/pages/Faq/FaqType.js
+++ total/front-end/src/pages/Faq/FaqType.js
@@ -7,10 +7,19 @@ function FaqType(props) {
   if (dbData !== []) {
     // console.log(dbData[0].faqType);
     return (
-      <Accordion defaultActiveKey="0" flush>
+      <Accordion
+        style={{
+          paddingTop: "40%",
+          width: "100%",
+          border: "none",
+          backgroundColor: "transparent",
+        }}
+        defaultActiveKey="0"
+        flush
+      >
         {dbData.map((data, index) => (
           <Accordion.Item key={index} eventKey={index.toString()}>
-            <Accordion.Header>{dbData[0].faqType}</Accordion.Header>
+            <Accordion.Header>{data.faqType}</Accordion.Header>
             <Accordion.Body>{data.body}</Accordion.Body>
           </Accordion.Item>
         ))}
