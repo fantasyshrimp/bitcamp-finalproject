c81fce71e92a5558283fe950a13e2c5f4ea02ac3
zyoonshin <dwsrh03@gmail.com>
2023-04-05 12:14:06 +0900

    connect db, add typing effict and show random image only

--- total/front-end/src/pages/Main/Card.js
+++ total/front-end/src/pages/Main/Card.js
@@ -1,42 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Card.module.css";
+import axios from "axios";
 
 function Card() {
+  const [no, setNo] = useState([]);
+  const [image, setImage] = useState([]);
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("http://localhost:8080/boards");
+        setImage(response.data);
+        setContent(response.data.contents);
+        console.log(image);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const randomImage = image[Math.floor(Math.random() * image.length)];
+
   return (
     <div className={styles.total}>
       <div className={styles.container}>
-        <div className={styles.card}>
-          <img
-            src="https://storage.googleapis.com/pai-images/b85f319d074c4a59ba41b38e0e21465d.jpeg"
-            alt="AImg"
-          />
-          <div className={styles.intro}>
-            <h1>image title1</h1>
-            <p>ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ</p>
+        {randomImage && (
+          <div className={styles.card}>
+            <img src={randomImage.fileName} />
+            <div className={styles.intro}>
+              {/* <p>{content[boardNo]}</p> */}
+            </div>
           </div>
-        </div>
-
-        <div className={styles.card}>
-          <img
-            src="https://storage.googleapis.com/pai-images/3fc14f2b063348ad95f55209876d344f.jpeg"
-            alt="AImg"
-          />
-          <div className={styles.intro}>
-            <h1>image title2</h1>
-            <p>ABCDEFGHIIIIIIIIIII</p>
-          </div>
-        </div>
-
-        <div className={styles.card}>
-          <img
-            src="https://storage.googleapis.com/pai-images/59038232dfd8497f9c945645f22b0095.jpeg"
-            alt="AImg"
-          />
-          <div className={styles.intro}>
-            <h1>image title3</h1>
-            <p>^&&13@#231244@@</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
