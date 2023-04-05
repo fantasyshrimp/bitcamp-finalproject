38883f2506441438787c96e1d2b12fd76b38c48c
zyoonshin <dwsrh03@gmail.com>
2023-04-05 14:11:14 +0900

    change Home

--- total/front-end/src/pages/Home.js
+++ total/front-end/src/pages/Home.js
@@ -1,37 +1,13 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import BackgroundSlider from "./Main/BackgroundSlider";
+import Card from "./Main/Card";
 
 function Home() {
-  const [hello, setHello] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/api/hello")
-      .then((response) => setHello(response.data))
-      .catch((error) => console.log(error));
-  }, []);
-
   return (
-    <Container fluid>
-      <Row className="bg-dark" style={{ height: "300px" }}></Row>
-      <Row className="bg-dark" style={{ height: "400px" }}>
-        <Col xl={2}></Col>
-        <Col xl={4} className="text-light">
-          <div style={{ fontSize: "150px" }}>{hello}</div>
-          <div>
-            <a
-              className="border text-light p-1"
-              style={{ textDecoration: "none" }}
-              href="#"
-            >
-              Try Artify
-            </a>
-          </div>
-        </Col>
-        <Col xl={6}></Col>
-      </Row>
-    </Container>
+    <div>
+      <BackgroundSlider />
+      <Card />
+    </div>
   );
 }
 
