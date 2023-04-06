98cf15ac8f8ad17e57ebc45a577b671d02c2c7a6
zyoonshin <dwsrh03@gmail.com>
2023-04-06 17:24:12 +0900

    edit mm modal

--- total/front-end/src/pages/MemberManagement/MemberList.js
+++ total/front-end/src/pages/MemberManagement/MemberList.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styles from "./MemberList.module.css";
-import { Modal, Button } from "react-bootstrap";
 import MemberView from "./MemberView";
 
 function MemberList() {
