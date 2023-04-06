c0ca93158194799693e58b252d4507e4b6451a89
zyoonshin <dwsrh03@gmail.com>
2023-04-06 12:41:26 +0900

    complete MemberList

--- total/front-end/src/pages/MemberManagement/MemberList.js
+++ total/front-end/src/pages/MemberManagement/MemberList.js
@@ -24,13 +24,7 @@ function MemberList() {
             <th>이메일</th>
             <th>비밀번호</th>
             <th>가입일</th>
-            <th>성별</th>
-            <th>프로필사진명</th>
-            <th>기본주소</th>
             <th>포인트</th>
-            <th>자기소개글</th>
-            <th>생년월일</th>
-            <th>전화번호</th>
             <th>비밀번호 변경일시</th>
             <th>계정상태</th>
             <th>권한레벨</th>
@@ -44,13 +38,7 @@ function MemberList() {
               <td>{member.email}</td>
               <td>{member.password}</td>
               <td>{member.createdDate}</td>
-              <td>{member.gender}</td>
-              <td>{member.profilePhoto}</td>
-              <td>{member.basicAddress}</td>
               <td>{member.point}</td>
-              <td>{member.information}</td>
-              <td>{member.birthDate}</td>
-              <td>{member.tel}</td>
               <td>{member.passwordDate}</td>
               <td>{member.accountState}</td>
               <td>{member.authLevel}</td>
