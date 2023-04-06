c237f6b5ef57b23289928cbc86edbbf5807a6f7e
zyoonshin <dwsrh03@gmail.com>
2023-04-06 09:54:47 +0900

    connect test.ver

--- total/front-end/src/components/Navbars.js
+++ total/front-end/src/components/Navbars.js
@@ -34,6 +34,7 @@ function Navbars() {
           <Nav className="me-auto">
             <Nav.Link href="/feed/">Feed</Nav.Link>
             <Nav.Link href="/faq/">FAQ</Nav.Link>
+            <Nav.Link href="/MemberList">MemberList</Nav.Link>
             <Searchs />
           </Nav>
 
