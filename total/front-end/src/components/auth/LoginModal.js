c35e878072d4ce6b5ee434cd8c91646255ad70c4
jongkwangyun <yjk0319@gmail.com>
2023-04-07 10:09:14 +0900

    알림창에서 게시물 클릭시 모달 띄우도록 구현. 로그인, 회원가입 오토포커스 구현

---
+++ total/front-end/src/components/auth/LoginModal.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import authBtnStyle from "./style";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+function LoginModal(props) {
+  const [show, setShow] = useState(true);
+  let { currentUser, setCurrentUser } = props;
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    props.handleShow();
+  };
+
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+
+  const handleClickLogin = (e) => {
+    const email = document.getElementsByName("email")[0].value;
+    if (!email.includes("@")) {
+      document.querySelector("#emailHelpBlock").innerText =
+        "이메일 형식이 올바르지 않습니다.";
+      setValidEmail(false);
+      return;
+    } else {
+      document.querySelector("#emailHelpBlock").innerText = "";
+      setValidEmail(true);
+    }
+
+    const password = document.getElementsByName("password")[0].value;
+    if (password.length === 0) {
+      document.querySelector("#passwordHelpBlock").innerText =
+        "비밀번호를 입력하세요";
+      setValidPassword(false);
+      document.getElementsByName("password")[0].focus();
+      return;
+    }
+    if (password.length === 10) {
+      setValidPassword(true);
+    } else {
+      document.querySelector("#passwordHelpBlock").innerText = "";
+    }
+
+    axios
+      .post(
+        "http://localhost:8080/auth/login",
+        {},
+        {
+          params: {
+            email: email,
+            password: password,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        if (response.data.status === "success") {
+          handleClose();
+          window.location.reload();
+        } else {
+          document.querySelector("#passwordHelpBlock").innerText =
+            "이메일 또는 비밀번호가 틀렸습니다.";
+        }
+      })
+      .catch((error) => {
+        alert("로그인 중 오류 발생");
+      });
+  };
+
+  const checkEmail = () => {
+    const email = document.getElementsByName("email")[0].value;
+    if (!email.includes("@")) {
+      setValidEmail(false);
+      return;
+    } else {
+      setValidEmail(true);
+    }
+  };
+
+  const handleChangePassword = () => {
+    if (document.getElementsByName("password")[0].value.length > 0) {
+      document.querySelector("#passwordHelpBlock").innerText = "";
+      setValidPassword(true);
+    } else {
+      document.querySelector("#passwordHelpBlock").innerText =
+        "비밀번호를 입력하세요";
+      setValidPassword(false);
+    }
+  };
+
+  const isDisabled = () => {
+    return !(validEmail && validPassword);
+  };
+
+  useEffect(() => {
+    // 모달 열렸을 때 오토포커스 주기
+    if (props.loginShow) {
+      const emailInput = document.getElementsByName("email")[0];
+      if (emailInput) {
+        emailInput.focus();
+      }
+    }
+  }, [props.loginShow]);
+
+  return (
+    <>
+      <Modal
+        show={props.loginShow}
+        onHide={handleClose}
+        centered
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#00000000",
+          border: "none",
+        }}
+        contentClassName="bg-dark"
+      >
+        <Modal.Header
+          closeButton
+          closeVariant="white"
+          style={{ borderBottom: "none" }}
+        ></Modal.Header>
+        <Modal.Header
+          style={{ borderBottom: "none" }}
+          className="d-flex justify-content-center p-0 pt-2 pb-2"
+        >
+          <Modal.Title className="text-light">반갑습니다!</Modal.Title>
+        </Modal.Header>
+
+        <Form>
+          <Modal.Body className="p-5 pb-4 pt-4">
+            <Form.Group className="mb-4" controlId="email">
+              <Form.Label className="text-light">
+                사용자의 이메일 주소를 입력해주세요
+              </Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="name@example.com"
+                className="bg-dark text-light"
+                onChange={checkEmail}
+              />
+              <Form.Text id="emailHelpBlock"></Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label className="text-light">
+                사용자의 비밀번호를 입력해주세요
+              </Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                className="bg-dark text-light"
+                onChange={handleChangePassword}
+              />
+              <Form.Text id="passwordHelpBlock"></Form.Text>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer
+            style={{ borderTop: "none" }}
+            className="d-flex justify-content-center pb-5 ps-5 pe-5"
+          >
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleClickLogin}
+              style={authBtnStyle}
+              disabled={isDisabled()}
+            >
+              Log In
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+}
+
+export default LoginModal;
---
+++ total/front-end/src/components/auth/SignupModal.js
@@ -0,0 +1,257 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal, Form, InputGroup } from "react-bootstrap";
+import authBtnStyle from "./style";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+function SignupModal(props) {
+  const [show, setShow] = useState(false);
+  let { currentUser, setCurrentUser } = props;
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [validEmail, setValidEmail] = useState(false);
+  const [validNickname, setValidNickname] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+  const [validConfirmPassword, setValidConfirmPassword] = useState(false);
+
+  function checkEmail() {
+    const email = document.getElementsByName("email")[0].value;
+    if (!email.includes("@")) {
+      document.querySelector("#emailHelpBlock").innerText =
+        "이메일 형식이 올바르지 않습니다.";
+      setValidEmail(false);
+      return;
+    }
+
+    axios
+      .get("http://localhost:8080/auth/checkemail", {
+        params: { email: email },
+      })
+      .then((response) => {
+        if (response.data.status === "success") {
+          document.querySelector("#emailHelpBlock").innerText =
+            "이미 가입된 이메일입니다.";
+          setValidEmail(false);
+        } else {
+          document.querySelector("#emailHelpBlock").innerText = "";
+          //   "사용가능한 이메일입니다.";
+          setValidEmail(true);
+        }
+      })
+      .catch((error) => {
+        alert("이메일 중복확인 중 오류 발생");
+      });
+  }
+
+  function blurEmail() {
+    if (validEmail) {
+      document.querySelector("#emailHelpBlock").innerText = "";
+    }
+  }
+
+  function checkNickname() {
+    const nickname = document.getElementsByName("nickname")[0].value;
+
+    axios
+      .get("http://localhost:8080/auth/checknickname", {
+        params: { nickname: nickname },
+      })
+      .then((response) => {
+        if (response.data.status === "success") {
+          document.querySelector("#nicknameHelpBlock").innerText =
+            "이미 사용중인 닉네임입니다.";
+          setValidNickname(false);
+        } else {
+          document.querySelector("#nicknameHelpBlock").innerText = "";
+          // "사용가능한 닉네임입니다.";
+          setValidNickname(true);
+        }
+      })
+      .catch((error) => {
+        alert("닉네임 중복확인 중 오류 발생");
+      });
+  }
+
+  function blurNickname() {
+    if (validNickname) {
+      document.querySelector("#nicknameHelpBlock").innerText = "";
+    }
+  }
+
+  function checkPasswordChar(e) {
+    const regex =
+      /^(?=.*[a-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+~`|}{[\]\\:';"<>,./?-]{10,}$/;
+    const isValid = regex.test(e.target.value);
+    if (isValid) {
+      document.querySelector("#passwordHelpBlock").innerText = "";
+      setValidPassword(true);
+    } else {
+      document.querySelector("#passwordHelpBlock").innerText =
+        "비밀번호는 영어, 숫자를 포함해 총 10글자 이상이어야 합니다.";
+      setValidPassword(false);
+    }
+  }
+
+  function checkBothPasswordSame(e) {
+    const password = document.getElementsByName("password")[0].value;
+    const passwordConfirm = e.target.value;
+
+    if (password === passwordConfirm) {
+      document.querySelector("#passwordConfirmHelpBlock").innerText = "";
+      setValidConfirmPassword(true);
+    } else {
+      document.querySelector("#passwordConfirmHelpBlock").innerText =
+        "비밀번호가 일치하지 않습니다.";
+      setValidConfirmPassword(false);
+    }
+  }
+
+  function handleSignupSubmit(e) {
+    const email = document.getElementsByName("email")[0].value;
+    const nickname = document.getElementsByName("nickname")[0].value;
+    const password = document.getElementsByName("password")[0].value;
+
+    axios
+      .post(
+        "http://localhost:8080/auth/signup",
+        {},
+        {
+          params: {
+            email: email,
+            nickname: nickname,
+            password: password,
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.status === "success") {
+          alert("가입이 완료 되었습니다");
+          handleClose();
+          window.location.reload();
+        } else {
+          alert("가입 양식이 맞지 않습니다");
+        }
+      })
+      .catch((error) => {
+        alert("회원가입 중 오류 발생");
+      });
+  }
+
+  const isDisabled = () => {
+    return !(
+      validEmail &&
+      validNickname &&
+      validPassword &&
+      validConfirmPassword
+    );
+  };
+
+  const focusEmailInput = () => {
+    const emailInput = document.getElementsByName("email")[0];
+    if (emailInput) {
+      emailInput.focus();
+    }
+  };
+
+  return (
+    <>
+      <div onClick={handleShow}>Sign up</div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        style={{ width: "100%", height: "100%", backgroundColor: "#00000000" }}
+        contentClassName="bg-dark"
+        onEntered={focusEmailInput}
+      >
+        <Modal.Header
+          closeButton
+          closeVariant="white"
+          style={{ borderBottom: "none" }}
+        ></Modal.Header>
+        <Modal.Header
+          style={{ borderBottom: "none" }}
+          className="d-flex justify-content-center p-0 pt-2 pb-2"
+        >
+          <Modal.Title className="text-light">회원가입</Modal.Title>
+        </Modal.Header>
+
+        <Form>
+          <Modal.Body className="p-5 pb-4 pt-4">
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label className="text-light">이메일</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="name@example.com"
+                  className="bg-dark text-light"
+                  onChange={checkEmail}
+                  onBlur={blurEmail}
+                />
+              </InputGroup>
+              <Form.Text id="emailHelpBlock"></Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="nickname">
+              <Form.Label className="text-light">닉네임</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type="text"
+                  name="nickname"
+                  placeholder="nickname"
+                  className="bg-dark text-light"
+                  onChange={checkNickname}
+                  onBlur={blurNickname}
+                />
+              </InputGroup>
+              <Form.Text id="nicknameHelpBlock"></Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label className="text-light">비밀번호</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                onChange={checkPasswordChar}
+                className="bg-dark text-light"
+              />
+              <Form.Text id="passwordHelpBlock"></Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password2">
+              <Form.Label className="text-light">비밀번호 확인</Form.Label>
+              <Form.Control
+                type="password"
+                name="passwordConfirm"
+                onChange={checkBothPasswordSame}
+                className="bg-dark text-light"
+              />
+              <Form.Text id="passwordConfirmHelpBlock"></Form.Text>
+            </Form.Group>
+          </Modal.Body>
+
+          <Modal.Footer
+            style={{ borderTop: "none" }}
+            className="d-flex justify-content-center pb-5 ps-5 pe-5"
+          >
+            <Button
+              variant="primary"
+              type="button"
+              onClick={handleSignupSubmit}
+              style={authBtnStyle}
+              disabled={isDisabled()}
+            >
+              Sign Up
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+}
+
+export default SignupModal;
