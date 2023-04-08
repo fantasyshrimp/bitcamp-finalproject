7cd0da0bcefb80059081c1b2ba246e79ae96c22c
jongkwangyun <yjk0319@gmail.com>
2023-04-08 16:48:33 +0900

    다크모드 로컬스토리지에 저장 구현

--- total/front-end/src/components/DarkModeSwitch.js
+++ total/front-end/src/components/DarkModeSwitch.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DarkModeSwitch.css";
 
 function DarkModeSwitch() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const setMode = () => {
+    const newMode = !isDarkMode;
+    localStorage.setItem("mode", newMode ? "dark" : "light");
+    setIsDarkMode(newMode);
+  };
+
+  const handleInputChange = () => {
+    setMode();
+  };
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem("mode");
+    setIsDarkMode(savedMode === "dark");
+  }, []);
+
   return (
     <>
       <div className="d-flex align-items-center me-3">
-        <label class="switch">
-          <input type="checkbox" />
-          <span class="slider"></span>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isDarkMode}
+            onChange={handleInputChange}
+          />
+          <span className="slider"></span>
         </label>
       </div>
     </>
