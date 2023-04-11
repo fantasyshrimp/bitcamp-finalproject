d3dea5d6ef58e30183fb7eda947df2462b83ed53
zyoonshin <dwsrh03@gmail.com>
2023-04-11 10:59:21 +0900

    edit home(login modal non-insert)

--- total/front-end/src/pages/Main/Video.js
+++ total/front-end/src/pages/Main/Video.js
@@ -3,7 +3,7 @@ import styles from "./Video.module.css";
 
 function Video() {
   return (
-    <div id="home" className={styles.home}>
+    <div className={styles.frame}>
       <video className={styles.video} autoPlay muted loop>
         <source src="video.mp4" type="video/mp4" />
       </video>
@@ -21,7 +21,7 @@ function Video() {
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <a href="https://www.naver.com/" className={`${styles.button} button`}>
-          Explore Now
+          ?클릭 시 로그인 모달 띄우기?
         </a>
       </div>
     </div>
