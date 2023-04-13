c810926a90408bb25e0c8345c63e0bcc0b86a562
fantasyshrimp <fantasy.shrimp@gmail.com>
2023-04-13 09:39:05 +0900

    .

--- total/back-end/src/main/java/bitcamp/app/service/AlarmService.java
+++ total/back-end/src/main/java/bitcamp/app/service/AlarmService.java
@@ -2,9 +2,11 @@ package bitcamp.app.service;
 
 import java.util.List;
 import bitcamp.app.vo.AlarmLog;
+import bitcamp.app.vo.Log;
 
 public interface AlarmService {
 
   List<AlarmLog> list(int no);
+  List<Log> getLogs(int memberNo);
 
 }
--- total/back-end/src/main/java/bitcamp/app/vo/Like.java
+++ total/back-end/src/main/java/bitcamp/app/vo/Like.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class Like {
-  private static final long serialVersionUID = 1L;
   private int contentNo;
   private int likerNo;
 }
