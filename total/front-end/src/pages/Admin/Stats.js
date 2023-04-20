5cfa9f5b48af02f5f1c43a2bd78eec8c8f0cf8f8
das7945 <das7945@naver.com>
2023-04-20 20:22:27 +0900

    방문자, 게시글, 댓글 조회기능 구현됨 _ 현재 UI진행중

--- total/front-end/src/pages/Admin/Stats.js
+++ total/front-end/src/pages/Admin/Stats.js
@@ -1,177 +1,71 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { ResponsiveLine } from "@nivo/line";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Visitor from "./Visitor";
+import BoardReply from "./BoardReply";
+import "./Stats.css";
 
-const Stats = () => {
-  const [chartData, setChartData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(() => {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    return yesterday;
-  });
-
-  const fetchData = async (date = new Date()) => {
-    const startDate = new Date(date);
-    startDate.setDate(startDate.getDate() - 6);
-    const endDate = new Date(date);
-    endDate.setDate(endDate.getDate() + 1);
-
-    try {
-      const response = await axios.get("http://localhost:8080/visitors", {
-        params: {
-          start_date: startDate.toISOString().split("T")[0],
-          end_date: endDate.toISOString().split("T")[0],
-        },
-      });
-      // console.log("endDate:", endDate.toISOString().split("T")[0]); 서버에서 받아온 데이터
-      // console.log("Response data:", response.data); // 응답 데이터 출력
-      const formattedData = formatData(response.data);
-      setChartData(formattedData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const formatData = (data) => {
-    const aggregatedData = data.reduce((acc, item) => {
-      const dateObj = new Date(item.visitorDt);
-      const date = `${dateObj.getFullYear()}-${String(
-        dateObj.getMonth() + 1
-      ).padStart(2, "0")}-${String(dateObj.getDate()).padStart(2, "0")}`;
-
-      if (!acc[date]) {
-        acc[date] = 1;
-      } else {
-        acc[date]++;
-      }
-      return acc;
-    }, {});
-
-    const chartData = [
-      {
-        id: "방문자",
-        color: "hsl(36, 70%, 50%)",
-        data: Object.entries(aggregatedData).map(([x, y]) => ({ x, y })),
-      },
-    ];
-
-    return chartData;
-  };
-
-  useEffect(() => {
-    fetchData(selectedDate);
-  }, [selectedDate]);
-
-  const handleDateChange = (event) => {
-    const dateParts = event.target.value.split("-");
-    const year = parseInt(dateParts[0], 10);
-    const month = parseInt(dateParts[1], 10) - 1; // month is 0-indexed
-    const day = parseInt(dateParts[2], 10);
-    const now = new Date();
-    const newDate = new Date(
-      year,
-      month,
-      day,
-      now.getHours(),
-      now.getMinutes(),
-      now.getSeconds()
-    );
-
-    console.log(
-      "선택된 날짜:",
-      newDate.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })
-    );
-    setSelectedDate(newDate);
-  };
-
-  // 오늘 날짜를 maxDate 변수에 저장
-  const maxDate = (() => {
-    const today = new Date();
-    today.setDate(today.getDate() - 1);
-    return today.toISOString().split("T")[0];
-  })();
+function Stats() {
+  const location = useLocation(); // 이 코드를 추가해 주세요.
+  const [menuNo, setMenuNo] = useState(
+    location.state ? location.state.menuNo : 0
+  );
+  const menu = ["방문자 주간조회", "콘텐츠 일별조회"];
 
   return (
-    <div>
-      <input
-        id="calendar"
-        type="date"
-        value={selectedDate.toISOString().split("T")[0]}
-        onChange={handleDateChange}
-        max={maxDate} // 최대 날짜를 사용자의 현재날짜 -1로 설정
-      />
+    <div
+      style={{
+        // backgroundColor: "blue",
+        display: "flex",
+        height: "83vh",
+        width: "100vw",
+      }}
+    >
       <div
-        style={{ height: "300px", backgroundColor: `var(--aim-text-default)` }}
+        style={{
+          width: "14.3%",
+          minWidth: "100px",
+          height: "100%",
+          marginLeft: "5%",
+          boxSizing: "border-box",
+          borderRight: `solid 1px var(--aim-border)`,
+          color: `var(--aim-text-default)`,
+        }}
       >
-        <ResponsiveLine
-          data={chartData}
-          margin={{ top: 30, right: 40, bottom: 70, left: 80 }}
-          xScale={{ type: "point" }}
-          yScale={{
-            type: "linear",
-            min: "auto",
-            max: "auto",
-            stacked: true,
-            reverse: false,
-          }}
-          yFormat=" >-.2f"
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            orient: "bottom",
-            tickSize: 15,
-            tickPadding: 0,
-            tickRotation: 0,
-            legend: "날짜",
-            legendOffset: 45,
-            legendPosition: "middle",
-          }}
-          axisLeft={{
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "조회수",
-            legendOffset: -40,
-            legendPosition: "middle",
-          }}
-          pointSize={10}
-          pointColor={{ theme: "background" }}
-          enableSlices="x"
-          pointBorderWidth={2}
-          pointBorderColor={{ from: "serieColor" }}
-          pointLabelYOffset={-12}
-          enableArea={false}
-          useMesh={true}
-          // legends={[
-          //   {
-          //     anchor: "top-left",
-          //     direction: "row",
-          //     justify: false,
-          //     translateX: 0,
-          //     translateY: -40,
-          //     itemsSpacing: 5,
-          //     itemDirection: "left-to-right",
-          //     itemWidth: 80,
-          //     itemHeight: 20,
-          //     itemOpacity: 0.75,
-          //     symbolSize: 15,
-          //     symbolShape: "circle",
-          //     symbolBorderColor: "rgba(0, 0, 0, .5)",
-          //     effects: [
-          //       {
-          //         on: "hover",
-          //         style: {
-          //           itemBackground: "rgba(0, 0, 0, .03)",
-          //           itemOpacity: 1,
-          //         },
-          //       },
-          //     ],
-          //   },
-          // ]}
-        />
+        <div style={{ height: "20%" }}></div>
+        <div style={{ marginRight: "5%" }}>
+          <h2
+            style={{
+              paddingBottom: "10%",
+              boxSizing: "border-box",
+            }}
+          >
+            통계
+          </h2>
+
+          {menu.map((title, index) => {
+            return (
+              <div
+                style={{
+                  // backgroundColor: "blue",
+                  paddingBottom: "5%",
+                }}
+                key={title + index}
+                onClick={() => {
+                  setMenuNo(index);
+                }}
+              >
+                {title}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="Stats-content">
+        {menuNo === 0 && <Visitor title={menu[0]} />}
+        {menuNo === 1 && <BoardReply title={menu[1]} />}
       </div>
     </div>
   );
-};
+}
+
 export default Stats;
