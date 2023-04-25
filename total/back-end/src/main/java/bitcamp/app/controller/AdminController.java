a583d1d30530eb2f3cb4bd3056425fe5e4ee53ad
zyoonshin <dwsrh03@gmail.com>
2023-04-25 13:42:37 +0900

    feat:making comment modal

--- total/back-end/src/main/java/bitcamp/app/controller/AdminController.java
+++ total/back-end/src/main/java/bitcamp/app/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -116,6 +117,12 @@ public class AdminController {
 
     return boardService.listTag(no);
   }
+
+  @DeleteMapping("/board/delete/{boardNo}")
+  public void delete(@PathVariable int boardNo, @RequestBody List<Integer> replyNos) {
+    boardService.deleteBoard(boardNo, replyNos);
+  }
+
 
 }
 
