af9cdbc5bc88bb439c32dc5203368ce244ae1708
das7945 <das7945@naver.com>
2023-04-26 20:07:52 +0900

    feat : 통계페이지 접속권한검사 기능추가_3

--- total/front-end/src/pages/Admin/Stats.js
+++ total/front-end/src/pages/Admin/Stats.js
@@ -15,7 +15,7 @@ function Stats(props) {
   );
   const menu = ["방문자 주간조회", "콘텐츠 일별조회"];
 
-  console.log("props.currentUser:", props.currentUser);
+  // console.log("props.currentUser:", props.currentUser);
 
   useEffect(() => {
     if (
@@ -32,95 +32,99 @@ function Stats(props) {
   }, [props.currentUser]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "95vh",
-        width: "100vw",
-        minWidth: "1200px",
-        // backgroundColor: "gray",
-      }}
-    >
-      <div
-        style={{
-          minWidth: "250px",
-          height: "100%",
-          boxSizing: "border-box",
-          borderRight: `solid 1px var(--aim-border)`,
-          color: `var(--aim-text-default)`,
-          // backgroundColor: "gray",
-        }}
-      >
+    <>
+      {props.currentUser && props.currentUser.authLevel === 9 && (
         <div
           style={{
-            height: "200px",
+            display: "flex",
+            height: "95vh",
+            width: "100vw",
+            minWidth: "1200px",
             // backgroundColor: "gray",
-          }}
-        ></div>
-        <div
-          style={{
-            marginLeft: "60px",
-            // backgroundColor: "blue",
           }}
         >
           <div
             style={{
-              fontSize: "35px",
-              paddingBottom: "5px",
-              marginLeft: "5px",
+              minWidth: "250px",
+              height: "100%",
               boxSizing: "border-box",
-              cursor: "default",
+              borderRight: `solid 1px var(--aim-border)`,
+              color: `var(--aim-text-default)`,
+              // backgroundColor: "gray",
             }}
           >
-            통계
+            <div
+              style={{
+                height: "200px",
+                // backgroundColor: "gray",
+              }}
+            ></div>
+            <div
+              style={{
+                marginLeft: "60px",
+                // backgroundColor: "blue",
+              }}
+            >
+              <div
+                style={{
+                  fontSize: "35px",
+                  paddingBottom: "5px",
+                  marginLeft: "5px",
+                  boxSizing: "border-box",
+                  cursor: "default",
+                }}
+              >
+                통계
+              </div>
+              <Nav
+                style={{
+                  marginLeft: "15px",
+                  // backgroundColor: "blue",
+                }}
+                className="flex-column"
+                defaultActiveKey="#"
+              >
+                {menu.map((title, index) => {
+                  return (
+                    <Nav.Link
+                      eventKey={index}
+                      className={`personalSetting-menu ${
+                        index === menuNo ? "active" : ""
+                      }`}
+                      href={index === 0 && "#"}
+                      style={{
+                        padding: "0px",
+                        // backgroundColor: "blue",
+                      }}
+                    >
+                      <div
+                        key={title + index}
+                        onClick={() => {
+                          setMenuNo(index);
+                        }}
+                        style={{
+                          fontSize: "16px",
+                          cursor: "pointer",
+                          padding: "5px",
+                          // borderBottom: `solid 1px var(--aim-border)`,
+                          // backgroundColor: "blue",
+                        }}
+                      >
+                        {title}
+                      </div>
+                    </Nav.Link>
+                  );
+                })}
+              </Nav>
+            </div>
           </div>
-          <Nav
-            style={{
-              marginLeft: "15px",
-              // backgroundColor: "blue",
-            }}
-            className="flex-column"
-            defaultActiveKey="#"
-          >
-            {menu.map((title, index) => {
-              return (
-                <Nav.Link
-                  eventKey={index}
-                  className={`personalSetting-menu ${
-                    index === menuNo ? "active" : ""
-                  }`}
-                  href={index === 0 && "#"}
-                  style={{
-                    padding: "0px",
-                    // backgroundColor: "blue",
-                  }}
-                >
-                  <div
-                    key={title + index}
-                    onClick={() => {
-                      setMenuNo(index);
-                    }}
-                    style={{
-                      fontSize: "16px",
-                      cursor: "pointer",
-                      padding: "5px",
-                      // borderBottom: `solid 1px var(--aim-border)`,
-                      // backgroundColor: "blue",
-                    }}
-                  >
-                    {title}
-                  </div>
-                </Nav.Link>
-              );
-            })}
-          </Nav>
+          <div id="Stats-content">
+            {menuNo === 0 && <Visitor title={menu[0]} />}
+            {menuNo === 1 && <BoardReply title={menu[1]} />}
+          </div>
         </div>
-      </div>
-      <div id="Stats-content">
-        {menuNo === 0 && <Visitor title={menu[0]} />}
-        {menuNo === 1 && <BoardReply title={menu[1]} />}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
